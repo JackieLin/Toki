ccfee358819f9028357ab4da8273d2c90c907498
megmei <513316897@qq.com>
2014-04-13 11:52:49 +0800

    add window alert and confirm panel file

---
+++ javascript/WindowPanel.js
@@ -0,0 +1,13 @@
+/**
+ * @author megmei
+ * @Date 2014-4-13
+ * @content rewrite window alter and confirm panel
+ */
+
+var WindowPanel = function( ){
+
+};
+
+WindowPanel.prototype.showPanel = function( ){
+
+};
